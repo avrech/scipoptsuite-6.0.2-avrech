db4659e72228a684832ffba089c0b565707f1b71
avrech <avrech@campus.technion.ac.il>
2020-07-14 07:43:42 +0300

    removing install dir

--- installation-dir/lib/cmake/soplex/soplex-config.cmake
+++
@@ -1,8 +0,0 @@
-if(NOT TARGET libsoplex)
-  include("${CMAKE_CURRENT_LIST_DIR}/soplex-targets.cmake")
-endif()
-
-set(SOPLEX_LIBRARIES libsoplex)
-set(SOPLEX_PIC_LIBRARIES libsoplex-pic)
-set(SOPLEX_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../../include")
-set(SOPLEX_FOUND TRUE)
